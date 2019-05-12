53e33959db6aed0dc7d64a7aef87d182a24d2e91
Jim Pivarski <jpivarski@gmail.com>
2019-05-11 21:20:40 -0500

    started on interpreter

--- pattern-match/define-and-run.py
+++ pattern-match/define-and-run.py
@@ -55,21 +55,46 @@ class AST:
         assert len(self._fields) == len(args)
         for n, x in zip(self._fields, args):
             setattr(self, n, x)
-            if self.line is None:
-                self.line = getattr(x, "line", None)
+            if not isinstance(x, list):
+                x = [x]
+            for xi in x:
+                if self.line is None:
+                    self.line = getattr(xi, "line", None)
 
     def __repr__(self):
         return "{0}({1})".format(type(self).__name__, ", ".join(getattr(self, n) for n in self._fields))
 
-class Literal(AST):
-    _fields = ("value",)
+class Module(AST):
+    _fields = ("statements",)
     def __str__(self):
-        return str(self.value)
+        return "\n".join(str(x) for x in self.statements)
 
-class Symbol(AST):
-    _fields = ("symbol",)
+class Match(AST):
+    _fields = ("patterns",)
     def __str__(self):
-        return self.symbol
+        return "\n    match " + "\n          ".join(str(x) for x in self.patterns)
+
+class Pattern(AST):
+    _fields = ("symbol", "expression", "cuts", "fits", "terminal")
+    def __str__(self):
+        constraints = [" cut " + str(x) for x in self.cuts] + [" fit " + str(x) for x in self.fits]
+        return "{0} {1} {2}{3}".format(self.symbol, "in" if self.terminal else "=", str(self.expression), "\n              ".join(constraints))
+
+class Assignment(AST):
+    _fields = ("symbol", "expression")
+    def __str__(self):
+        return "{0} = {1}".format(self.symbol, str(self.expression))
+
+class Function(AST):
+    _fields = ("parameters", "body")
+    def __str__(self):
+        left, right = ("", "") if len(self.parameters) == 1 else ("(", ")")
+        return "{0}{1}{2} => {3}".format(left, ", ".join(self.parameters), right, str(self.body))
+
+class Block(AST):
+    _fields = ("statements", "expression")
+    def __str__(self):
+        return "{" + "; ".join(str(x) for x in self.statements + [self.expression]) + "}"
 
 class Call(AST):
     _fields = ("function", "arguments")
@@ -86,37 +111,80 @@ class Attribute(AST):
     def __str__(self):
         return "{0}.{1}".format(str(self.object), self.field)
 
-class Module(AST):
-    _fields = ("statements",)
+class Symbol(AST):
+    _fields = ("symbol",)
     def __str__(self):
-        return "\n".join(str(x) for x in self.statements)
+        return self.symbol
 
-class Block(AST):
-    _fields = ("statements", "expression")
+class Literal(AST):
+    _fields = ("value",)
     def __str__(self):
-        return "{" + "; ".join(str(x) for x in self.statements + [self.expression]) + "}"
+        return str(self.value)
 
-class Function(AST):
-    _fields = ("parameters", "body")
-    def __str__(self):
-        left, right = ("", "") if len(self.parameters) == 1 else ("(", ")")
-        return "{0}{1}{2} => {3}".format(left, ", ".join(self.parameters), right, str(self.body))
+opname = {
+    "and": "and", "or": "or", "not": "not",
+    "eq": "==", "ne": "!=", "gt": ">", "ge": ">=", "lt": "<", "le": "<=",
+    "add": "+", "sub": "-", "mul": "*", "div": "/", "pos": "+", "neg": "-", "pow": "**"
+    }
 
-class Assignment(AST):
-    _fields = ("symbol", "expression")
-    def __str__(self):
-        return "{0} = {1}".format(self.symbol, str(self.expression))
+def toast(node):
+    if node.data == "pass":
+        return toast(node.children[0])
+    elif node.data == "start":
+        return Module([toast(x) for x in node.children if not isinstance(x, lark.lexer.Token)])
+    elif node.data == "match":
+        return Match([toast(x) for x in node.children])
+    elif node.data == "intermediate" or node.data == "terminal":
+        cuts = [toast(x.children[0]) for x in node.children[2:] if x.data == "cut"]
+        fits = [toast(x.children[0]) for x in node.children[2:] if x.data == "fit"]
+        return Pattern(str(node.children[0]), toast(node.children[1]), cuts, fits, node.data == "terminal", line=node.children[0].line)
+    elif node.data == "assignment":
+        return Assignment(str(node.children[0]), toast(node.children[1]), line=node.children[0].line)
+    elif node.data == "funcassign":
+        return Assignment(str(node.children[0]), Function(node.children[1:-1], toast(node.children[-1])), line=node.children[0].line)
+    elif node.data == "function":
+        return Function(toast(node.children[0]), toast(node.children[1]), line=node.children[0].line)
+    elif node.data == "paramlist":
+        return [str(x) for x in node.children]
+    elif node.data == "block":
+        items = [toast(x) for x in node.children if not isinstance(x, lark.lexer.Token)]
+        if len(items) == 1:
+            return items[0]
+        else:
+            return Block(items[:-1], items[-1])
+    elif node.data == "call":
+        return Call(toast(node.children[0]), toast(node.children[1]))
+    elif node.data == "arglist":
+        return [toast(x) for x in node.children]
+    elif node.data == "subscript":
+        return Subscript(toast(node.children[0]), toast(node.children[1]))
+    elif node.data == "attribute":
+        return Attribute(toast(node.children[0]), node.children[1])
+    elif node.data == "symbol":
+        return Symbol(node.children[0], line=node.children[0].line)
+    elif node.data == "int":
+        return Literal(int(node.children[0]), line=node.children[0].line)
+    elif node.data == "float":
+        return Literal(float(node.children[0]), line=node.children[0].line)
+    elif node.data in opname:
+        return Call(Symbol(opname[node.data]), [toast(x) for x in node.children])
+    else:
+        raise AssertionError(node.data)
 
-class Pattern(AST):
-    _fields = ("symbol", "expression", "cuts", "fits", "terminal")
-    def __str__(self):
-        constraints = [" cut " + str(x) for x in self.cuts] + [" fit " + str(x) for x in self.fits]
-        return "{0} {1} {2}{3}".format(self.symbol, "in" if self.terminal else "=", str(self.expression), "\n              ".join(constraints))
-
-class Match(AST):
-    _fields = ("patterns",)
-    def __str__(self):
-        return "\n    match " + "\n          ".join(str(x) for x in self.patterns)
+# print(toast(parser.parse("""
+# higgs =
+#     match h = z1 + z2 fit (h.mass - 125)**2
+#           z1 = mu1 + mu2
+#               cut mu1.charge != mu2.charge
+#               fit (z1.mass - 91)**2
+#           mu1 in muons
+#           mu2 in muons
+#           z2 = mu3 + mu4
+#               cut mu3.charge != mu4.charge
+#               fit (z2.mass - 91)**2
+#           mu3 in muons
+#           mu4 in muons
+# """)))
 
 class SymbolTable:
     def __init__(self, parent, symbols):
@@ -145,68 +213,99 @@ class SymbolTable:
         else:
             self.symbols[symbol] = value
 
-opname = {
-    "and": "and", "or": "or", "not": "not",
-    "eq": "==", "ne": "!=", "gt": ">", "ge": ">=", "lt": "<", "le": "<=",
-    "add": "+", "sub": "-", "mul": "*", "div": "/", "pos": "+", "neg": "-", "pow": "**"
-    }
+    def __str__(self):
+        return str(self.symbols)
 
-def toast(node):
-    if node.data == "pass":
-        return toast(node.children[0])
-    elif node.data == "start":
-        return Module([toast(x) for x in node.children if not isinstance(x, lark.lexer.Token)])
-    elif node.data == "match":
-        return Match([toast(x) for x in node.children])
-    elif node.data == "intermediate" or node.data == "terminal":
-        cuts = [toast(x.children[0]) for x in node.children[2:] if x.data == "cut"]
-        fits = [toast(x.children[0]) for x in node.children[2:] if x.data == "fit"]
-        return Pattern(str(node.children[0]), toast(node.children[1]), cuts, fits, node.data == "terminal")
-    elif node.data == "assignment":
-        return Assignment(str(node.children[0]), toast(node.children[1]))
-    elif node.data == "funcassign":
-        return Assignment(str(node.children[0]), Function(node.children[1:-1], toast(node.children[-1])))
-    elif node.data == "function":
-        return Function(toast(node.children[0]), toast(node.children[1]))
-    elif node.data == "paramlist":
-        return [str(x) for x in node.children]
-    elif node.data == "block":
-        items = [toast(x) for x in node.children if not isinstance(x, lark.lexer.Token)]
-        if len(items) == 1:
-            return items[0]
-        else:
-            return Block(items[:-1], items[-1])
-    elif node.data == "call":
-        return Call(toast(node.children[0]), toast(node.children[1]))
-    elif node.data == "arglist":
-        return [toast(x) for x in node.children]
-    elif node.data == "subscript":
-        return Subscript(toast(node.children[0]), toast(node.children[1]))
-    elif node.data == "attribute":
-        return Attribute(toast(node.children[0]), node.children[1])
-    elif node.data == "symbol":
-        return Symbol(node.children[0])
-    elif node.data == "int":
-        return Literal(int(node.children[0]))
-    elif node.data == "float":
-        return Literal(float(node.children[0]))
-    elif node.data in opname:
-        return Call(Symbol(opname[node.data]), [toast(x) for x in node.children])
+def run(node, symbols):
+    if isinstance(node, Module):
+        for x in node.statements:
+            run(x, symbols)
+    elif isinstance(node, Assignment):
+        symbols[node.symbol] = run(node.expression, symbols)
+    elif isinstance(node, Call):
+        try:
+            return run(node.function, symbols)(node.arguments, symbols)
+        except Exception as err:
+            raise RuntimeError("on line {0}, encountered {1}: {2}".format("???" if node.line is None else node.line, type(err).__name__, str(err)))
+    elif isinstance(node, Symbol):
+        return symbols[node.symbol]
+    elif isinstance(node, Literal):
+        return node.value
     else:
-        raise AssertionError(node.data)
+        raise AssertionError(type(node))
 
-print(toast(parser.parse("""
-higgs =
-    match h = z1 + z2 fit (h.mass - 125)**2
-          z1 = mu1 + mu2
-              cut mu1.charge != mu2.charge
-              fit (z1.mass - 91)**2
-          mu1 in muons
-          mu2 in muons
-          z2 = mu3 + mu4
-              cut mu3.charge != mu4.charge
-              fit (z2.mass - 91)**2
-          mu3 in muons
-          mu4 in muons
-""")))
+builtins = SymbolTable(None, {
+    "and": lambda arguments, symbols: run(arguments[0], symbols) and run(arguments[1], symbols),
 
+    "+": lambda arguments, symbols: run(arguments[0], symbols) + run(arguments[1], symbols),
+
+
+    # "and": "and", "or": "or", "not": "not",
+    # "eq": "==", "ne": "!=", "gt": ">", "ge": ">=", "lt": "<", "le": "<=",
+    # "add": "+", "sub": "-", "mul": "*", "div": "/", "pos": "+", "neg": "-", "pow": "**"
+    })
+
+symbols = SymbolTable(builtins, {})
+
+run(toast(parser.parse("""
+x = 2 + 2
+""")), symbols)
+
+print(symbols)
+
+# class Module(AST):
+#     _fields = ("statements",)
+#     def __str__(self):
+#         return "\n".join(str(x) for x in self.statements)
+
+# class Match(AST):
+#     _fields = ("patterns",)
+#     def __str__(self):
+#         return "\n    match " + "\n          ".join(str(x) for x in self.patterns)
+
+# class Pattern(AST):
+#     _fields = ("symbol", "expression", "cuts", "fits", "terminal")
+#     def __str__(self):
+#         constraints = [" cut " + str(x) for x in self.cuts] + [" fit " + str(x) for x in self.fits]
+#         return "{0} {1} {2}{3}".format(self.symbol, "in" if self.terminal else "=", str(self.expression), "\n              ".join(constraints))
+
+# class Assignment(AST):
+#     _fields = ("symbol", "expression")
+#     def __str__(self):
+#         return "{0} = {1}".format(self.symbol, str(self.expression))
+
+# class Function(AST):
+#     _fields = ("parameters", "body")
+#     def __str__(self):
+#         left, right = ("", "") if len(self.parameters) == 1 else ("(", ")")
+#         return "{0}{1}{2} => {3}".format(left, ", ".join(self.parameters), right, str(self.body))
+
+# class Block(AST):
+#     _fields = ("statements", "expression")
+#     def __str__(self):
+#         return "{" + "; ".join(str(x) for x in self.statements + [self.expression]) + "}"
+
+# class Call(AST):
+#     _fields = ("function", "arguments")
+#     def __str__(self):
+#         return "{0}({1})".format(str(self.function), ", ".join(str(x) for x in self.arguments))
+
+# class Subscript(AST):
+#     _fields = ("object", "index")
+#     def __str__(self):
+#         return "{0}[{1}]".format(str(self.object), str(self.index))
+
+# class Attribute(AST):
+#     _fields = ("object", "field")
+#     def __str__(self):
+#         return "{0}.{1}".format(str(self.object), self.field)
+
+# class Symbol(AST):
+#     _fields = ("symbol",)
+#     def __str__(self):
+#         return self.symbol
+
+# class Literal(AST):
+#     _fields = ("value",)
+#     def __str__(self):
+#         return str(self.value)
